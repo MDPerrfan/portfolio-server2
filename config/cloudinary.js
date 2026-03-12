09e35cabc608ab366bd521838232f0caa3daa166
Mohammed Parves <mdperrfan@gmail.com>
2026-03-12 20:00:33 +0600

    configured cloudinary

---
+++ config/cloudinary.js
@@ -0,0 +1,20 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: 'portfolio',
+        allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
+        transformation: [{ width: 800, quality: "auto", crop: 'limit' }]
+    },
+});
+
+export const upload = multer({ storage });
+export { cloudinary };
