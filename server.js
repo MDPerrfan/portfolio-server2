049872d129eed9402ef6161cfebae022a8d39cf2
Mohammed Parves <mdperrfan@gmail.com>
2026-03-12 19:21:47 +0600

    Backend Structe and initial start

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Portfolio API' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
